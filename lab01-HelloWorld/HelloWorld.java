72344a2b997d9b95682b9a2adfe161c0c250d3e1
namin14 <nea314@lehigh.edu>
2015-01-23 22:17:44 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+// Nehla Amin
+// Demo 0
+// Hello World Java Program
+//
+// First compile the program
+//      javac HelloWorld.java
+// Run the Program
+//      java HelloWorld
+
+// Define a class
+public class HelloWorld{
+
+// Add main method    
+    public static void main(String[] args){
+        
+ // Print the statements
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#   Hello World Java   #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+    
+    
+}
